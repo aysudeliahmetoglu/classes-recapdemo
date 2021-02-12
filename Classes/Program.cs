60704357f78954e3d3d89fc12b68790eec594adf
aysudeliahmetoglu <aysudeliahmetoglu@gmail.com>
2021-02-12 22:53:24 +0300

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+            Customer customer = new Customer();
+            customer.FirstName = "Engin";
+
+            Customer customer2 = new Customer
+            {
+                Id=2, City="ankara"
+            };
+            Console.WriteLine(customer2.Id +" " + customer2.City);
+            Console.ReadLine();
+        }
+    }
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Customer updated");
+        }
+
+    }
+}
